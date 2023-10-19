ffb32e00d3823bc2b70bea3d4025777b03b4adce
Ben Rehmann <brehmann@me.com>
2023-10-19 14:02:52 -0500

    reset, will come back later to this

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -9,25 +9,6 @@ const pool = require('../modules/pool.js');
 
 // GET response /todo
 
-router.get(`/`, (req, res) => {
-    console.log(`GET /todo request matched`);
-
-// queryText for SQL
-    let queryText = `
-    SELECT * FROM "tasks";
-    `;
-
-// send your query to SQL, if functional, run .then, if not .catch
-    pool.query(queryText).then((result) => {
-        console.log(`GET query /todo Success`);
-// upon success send the resulting rows from SQL
-        res.sendStatus(result.rows);
-    }).catch((error) => {
-        console.log(`GET query /todo ERROR`);
-        res.sendStatus(500);
-    });
-});
-
 // POST
 
 // PUT
